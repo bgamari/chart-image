631882d7538f416a7adee208b70c3290e565417e
Ben Gamari <bgamari.foss@gmail.com>
2012-05-10 13:56:32 -0400

    Implement nearest neighbor resampling

--- Graphics/Rendering/Chart/Plot/Image.hs
+++ Graphics/Rendering/Chart/Plot/Image.hs
@@ -2,6 +2,7 @@
 
 module Graphics.Rendering.Chart.Plot.Image ( Image
                                            , defaultImage
+                                           , ResampleType(..)
                                            , image_data
                                            , image_extent
                                            ) where
@@ -23,14 +24,19 @@ import           Data.Accessor.Template
 
 import           Control.Monad
 
-data Image r e x y = Image { image_data_    :: A.Array r A.DIM2 e
-                           , image_extent_  :: ((x,y), (x,y))
+data ResampleType = NearestNeighbor
+                  deriving (Show, Eq)
+
+data Image r e x y = Image { image_data_          :: A.Array r A.DIM2 e
+                           , image_extent_        :: ((x,y), (x,y))
+                           , image_resample_type_ :: ResampleType
                            }
      
 defaultImage :: (Fillable r e, PlotValue x, PlotValue y) => Image r e x y
 defaultImage = Image
-  { image_data_    = fromList (A.Z :. 0 :. 0) []
-  , image_extent_  = ((fromValue 0, fromValue 0), (fromValue 1, fromValue 1))
+  { image_data_          = fromList (A.Z :. 0 :. 0) []
+  , image_extent_        = ((fromValue 0, fromValue 0), (fromValue 1, fromValue 1))
+  , image_resample_type_ = NearestNeighbor
   }
   
 instance (ImageData e, A.Repr r e) => ToPlot (Image r e) where
@@ -50,11 +56,11 @@ instance ImageData (Double,Double,Double) where
     stride <- C.imageSurfaceGetStride surf
     d <- C.imageSurfaceGetPixels surf :: IO (C.SurfaceData Int Word32)
     forM_ [0..w-1] $ \x->
-      forM_ [0..h-1] $ \y->do
-        let (r,g,b) = a A.! (A.ix2 x y)
-        DA.writeArray d (x+y*(stride `div` 4)) $ round (255*r) `shiftL` 16
-                                   .|. round (255*g) `shiftL` 8
-                                   .|. round (255*b)
+      forM_ [0..h-1] $ \y->
+        let (r,g,b) = a A.! A.ix2 x y
+        in DA.writeArray d (x+y*(stride `div` 4)) $ round (255*r) `shiftL` 16
+                                                .|. round (255*g) `shiftL` 8
+                                                .|. round (255*b)
     C.surfaceMarkDirty surf
     return surf
 
@@ -62,13 +68,27 @@ vflip :: A.Repr r e => A.Array r A.DIM2 e -> A.Array A.D A.DIM2 e
 vflip a = A.backpermute (A.extent a) (\(A.Z :. x :. y)->A.ix2 x (h-1-y)) a
   where A.Z :. w :. h = A.extent a
 
+resampleNearest :: (A.Repr r e, ImageData e) => (Int, Int) -> A.Array r A.DIM2 e -> A.Array A.D A.DIM2 e
+resampleNearest (w',h') a =
+  let A.Z :. w :. h = A.extent a
+      samplePoint (A.Z :. x :. y) = A.ix2 (round $ realToFrac (x * (w-1)) / realToFrac (w'-1))
+                                          (round $ realToFrac (y * (h-1)) / realToFrac (h'-1))
+  in A.backpermute (A.ix2 w' h') samplePoint a
+
+resample :: (A.Repr r e, ImageData e) => ResampleType -> (Int, Int) -> A.Array r A.DIM2 e -> A.Array A.D A.DIM2 e
+resample NearestNeighbor = resampleNearest
+
 renderImage  :: (A.Repr r e, ImageData e) => Image r e x y -> PointMapFn x y -> CRender ()
 renderImage p pmap = preserveCState $ c $ do
-    surf <- C.liftIO $ copyArrayToSurface $ vflip $ image_data_ p
-    C.setSourceSurface surf 0 0
     let ((x0,y0), (x1,y1)) = image_extent_ p
         Point x0' y0' = pmap (LValue x0, LValue y0)
         Point x1' y1' = pmap (LValue x1, LValue y1)
+    (w,h) <- C.userToDeviceDistance (x1'-x0') (y0'-y1')
+    C.liftIO $ print (w,h)
+    surf <- C.liftIO $ copyArrayToSurface
+                     $ resample (image_resample_type_ p) (round w, round h)
+                     $ vflip $ image_data_ p
+    C.setSourceSurface surf 0 0
     C.rectangle x0' y0' (x1'-x0') (y1'-y0')
     C.fill
   
